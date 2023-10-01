0bc3775db459b3fc8332f92a7f4eaec3466fc598
qcandymanp <hendrikherpers@gmail.com>
2023-10-01 21:26:09 +0200

    Adjust Game-Model to save current position in FEN and the latest move in PGN

--- Models/Game.cs
+++ Models/Game.cs
@@ -3,5 +3,11 @@ namespace ChessApi.Models;
 public class Game
 {
     public Guid id { get; set; }
-    public string? pgn { get; set; }
+    public string? position { get; set; } // FEN-notation
+    public string? history { get; set; } // PGN-notation
+
+    public Game()
+    {
+        this.position = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    }
 }
