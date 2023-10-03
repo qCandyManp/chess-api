9c0b5bd883af130e2f5129edf94cdc31104a37ed
Hendrik Herpers <hherpers@network-publishing.de>
2023-10-03 14:36:38 +0200

    Implement constructor for GameState

--- Services/Helpers/GameState.cs
+++ Services/Helpers/GameState.cs
@@ -3,9 +3,26 @@ namespace ChessApi.Services.Helpers;
 class GameState
 {
     public Dictionary<string, Piece> Field;
+    public char MoveRight;
+    public string CastlingRights;
+    public string EnPassant;
+    public int Plies;
+    public int Moves;
 
-    public GameState(string position)
+    public GameState(
+        Dictionary<string, Piece> field,
+        char moveRight,
+        string castlingRights,
+        string enPassant,
+        int plies,
+        int moves
+    )
     {
-        Field = new Dictionary<string, Piece>();
+        Field = field;
+        MoveRight = moveRight;
+        CastlingRights = castlingRights;
+        EnPassant = enPassant;
+        Plies = plies;
+        Moves = moves;
     }
 }
